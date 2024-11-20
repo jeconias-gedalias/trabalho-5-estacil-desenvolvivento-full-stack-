66cdf7351b192bbad54f6d372a076ca554212be1
jeconias-gedalias <152023736+jeconias-gedalias@users.noreply.github.com>
2024-11-19 22:46:32 -0300

    Update Livro.ts

--- trabalho5estacio/trabalho5estacio/clientes/livros-next/classes/modelo/Livro.ts
+++ trabalho5estacio/trabalho5estacio/clientes/livros-next/classes/modelo/Livro.ts
@@ -1,12 +1,12 @@
 class Livro {
-    _id?: string; // Identificador opcional do MongoDB, do tipo string
+    _id?: string; 
     codEditora: number;
     titulo: string;
     resumo: string;
     autores: string[];
 
     constructor(codEditora: number, titulo: string, resumo: string, autores: string[], _id?: string) {
-        this._id = _id; // Opcional, pois pode não ser fornecido na criação
+        this._id = _id; 
         this.codEditora = codEditora;
         this.titulo = titulo;
         this.resumo = resumo;
