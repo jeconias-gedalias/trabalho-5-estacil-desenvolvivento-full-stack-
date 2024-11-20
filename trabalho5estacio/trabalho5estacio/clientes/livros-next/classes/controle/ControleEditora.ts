8618319d8714edb4a8151e1ad033b4ffaf7225ec
jeconias-gedalias <152023736+jeconias-gedalias@users.noreply.github.com>
2024-11-19 22:45:10 -0300

    Update ControleEditora.ts

--- trabalho5estacio/trabalho5estacio/clientes/livros-next/classes/controle/ControleEditora.ts
+++ trabalho5estacio/trabalho5estacio/clientes/livros-next/classes/controle/ControleEditora.ts
@@ -13,12 +13,12 @@ class ControleEditora {
         this.editoras = editoras;
     }
 
-    // Método para retornar o vetor editoras
+  
     public getEditoras(): Array<Editora> {
         return this.editoras;
     }
 
-    // Método para retornar o nome da editora pelo codEditora
+
     public getNomeEditora(codEditora: number): String | undefined {
         const editora = this.editoras.filter(e => e.codEditora === codEditora);
         return editora.length > 0 ? editora[0].nome : undefined;
