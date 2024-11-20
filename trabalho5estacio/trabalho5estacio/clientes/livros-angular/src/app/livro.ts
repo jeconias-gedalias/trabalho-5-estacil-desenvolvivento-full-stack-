1a17cb15709cc46327e43e73c7eee2d0e6568918
jeconias-gedalias <152023736+jeconias-gedalias@users.noreply.github.com>
2024-11-19 22:42:59 -0300

    Update livro.ts

--- trabalho5estacio/trabalho5estacio/clientes/livros-angular/src/app/livro.ts
+++ trabalho5estacio/trabalho5estacio/clientes/livros-angular/src/app/livro.ts
@@ -1,12 +1,12 @@
 export class Livro {
-    _id?: string; // Identificador opcional do MongoDB, do tipo string
+    _id?: string; 
     codEditora: number;
     titulo: string;
     resumo: string;
     autores: string[];
 
     constructor(_id?: string, codEditora: number = 0, titulo: string = '', resumo: string = '', autores: string[] = []) {
-        this._id = _id; // Opcional, pois pode não ser fornecido na criação
+        this._id = _id; 
         this.codEditora = codEditora;
         this.titulo = titulo;
         this.resumo = resumo;
